664029e41421d15f05edaf3888e2f9aff01e9f32
combatcellist <combatcellist913@gmail.com>
2021-01-30 16:41:48 +0900

    レイアウト調整

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,39 @@
+class ItemsController < ApplicationController
+
+  # before_action :find_item, only: [:edit, :]
+
+  def index
+    @items = Item.all.order(created_at: :desc)
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path, notice: "投稿されました"
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+end
